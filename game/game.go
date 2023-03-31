623cf197c197f1a9acc617ad253afc06589935de
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2023-03-31 12:46:43 +0330

    Work with receiver and interface

---
+++ game/game.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type point struct {
+	X int
+	Y int
+}
+
+func main() {
+	game()
+}
+
+func game() {
+	var p1 point
+	fmt.Println(p1)
+	fmt.Printf("%#v\n", p1)
+
+	p2 := point{1, 2}
+	fmt.Printf("%#v\n", p2)
+
+	p3, _ := newPoint(10, 20)
+	p3.move(20, 10)
+	p3.status()
+}
+
+func (p *point) status() {
+	fmt.Printf("You're in (%d, %d)", p.X, p.Y)
+}
+
+func (p *point) move(x, y int) {
+	p.X = x
+	p.Y = y
+}
+
+const (
+	maxX = 1000
+	maxY = 700
+)
+
+func newPoint(x, y int) (*point, error) {
+	if x < 0 || x > maxX || y < 0 || y > maxY {
+		return nil, fmt.Errorf("%d, %d out of bounds %d, %d", x, y, maxX, maxY)
+	}
+	return &point{
+		X: x,
+		Y: y,
+	}, nil
+}
