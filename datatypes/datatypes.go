addc7c6227b2ddac4a5a261fec2581434c3bcd29
Ishmael MIRZAEE <IshmaelMIRZAEE@gmail.com>
2023-04-03 16:16:21 +0330

    Work with receivers and responds

--- datatypes/datatypes.go
+++ datatypes/datatypes.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/esmaeilmirzaee/practical_go/datatypes/organization"
 )
 
 func main() {
 	var p organization.Identifiable = &organization.Person{}
+	var p2 = organization.NewPerson("John", "Doe")
+	println(p2.FullName())
 	println(p.ID())
+
+	handler := "@esmaeilmirzaee"
+	if err := p2.SetHandler(handler); err != nil {
+		fmt.Printf("%s", err.Error())
+	}
+	println("Twitter handler: ", p2.TwitterHandler())
 }
--- datatypes/organization/person.go
+++ datatypes/organization/person.go
@@ -1,13 +1,20 @@
 package organization
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Identifiable interface {
 	ID() string
 }
 
 type Person struct {
-	firstName string
-	lastName  string
-	Age       int
+	firstName      string
+	lastName       string
+	Age            int
+	twitterHandler string
 }
 
 func NewPerson(firstName, lastName string) Person {
@@ -17,6 +24,23 @@ func NewPerson(firstName, lastName string) Person {
 	}
 }
 
+func (p *Person) FullName() string {
+	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
+}
+
 func (p *Person) ID() string {
 	return "1234."
+}
+
+func (p *Person) SetHandler(handler string) error {
+	if !strings.HasPrefix(handler, "@") {
+		return errors.New("handler should start with '@'")
+	}
+
+	p.twitterHandler = handler
+	return nil
+}
+
+func (p *Person) TwitterHandler() string {
+	return p.twitterHandler
 }
